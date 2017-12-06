f58ac55688304558bed89793d61eefeed3cb1882
strkita <kita83@gmail.com>
2017-12-06 23:00:15 +0900

    accounts/loginルーティング追加

--- feed/urls.py
+++ feed/urls.py
@@ -1,4 +1,4 @@
-import django.contrib.auth.views
+from allauth import account
 from django.conf.urls import url
 
 from . import views
@@ -6,6 +6,7 @@ from . import views
 app_name = 'feed'
 urlpatterns = [
     url(r'$', views.index, name='index'),
-    url(r'^login/$', django.contrib.auth.views.login, {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/$', django.contrib.auth.views.logout, {'template_name': 'logout.html'}, name='logout'),
+    url(r'^signup/?$', views.SignupView, name='signup'),
+    url(r'^login/$', views.LoginView, name='login'),
+    url(r'^logout/$', views.LogoutView, name='logout'),
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,6 @@
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from allauth.account import views
 
 
 def index(request):
@@ -18,3 +19,41 @@ def index(request):
     }
 
     return render(request, 'feed/index.html', context)
+
+
+class LoginView(views.LoginView):
+    template_name = 'feed/login.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        response = super(LoginView, self).dispatch(request, *args, **kwargs)
+        return response
+
+    def form_valid(self, form):
+        return super(LoginView, self).form_valid(form)
+
+
+login = LoginView.as_view()
+
+
+class LogoutView(views.LogoutView):
+    def get(self, *args, **kwargs):
+        return self.post(*args, **kwargs)
+
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated():
+            self.logout()
+        return redirect('/')
+
+
+logout = LogoutView.as_view()
+
+
+class SignupView(views.SignupView):
+    template_name = 'feed/signup.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SignupView, self).get_context_data(**kwargs)
+        return context
+
+
+signup_view = SignupView.as_view()
