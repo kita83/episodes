c7e6299c66cae37bc55eab5ffad6860005c942c9
strkita <kita83@gmail.com>
2017-12-09 09:09:00 +0900

    SB2-admin-2適用

--- feed/urls.py
+++ feed/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     url(r'^signup/?$', views.signup, name='signup'),
     url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', views.logout, name='logout'),
-    url(r'^accounts/profile/$', TemplateView.as_view(template_name='profile.html')),
 ]
