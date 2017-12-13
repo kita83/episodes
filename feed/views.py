ee07254ecff929d63ee5a54934ea1ff1b2991167
kita83 <kita83@gmail.com>
2017-12-13 19:46:59 +0900

    feedデータをview側で処理するように変更

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,4 @@
+import pprint
 import requests
 from django.shortcuts import render, redirect
 from allauth.account import views
@@ -6,16 +7,38 @@ from allauth.account import views
 def index(request):
     """
     トレンド一覧画面
+
+    iTunes APIからPodcastランキングを取得する
     :param request:
-    :return: feeds:
+    :return: context:
     """
 
-    # iTunesストアからPodcastランキング情報を取得
+    # ジャンル"Software How-To"のPodcastランキング情報を取得
     url = 'https://itunes.apple.com/jp/rss/topaudiopodcasts/genre=1480/json'
     res = requests.get(url).json()
 
+    # 表示用データ抽出
+    feeds = []
+    for feed in res['feed']['entry']:
+        # エピソードタイトル
+        title = 'episode title'
+        # チャンネルタイトル
+        channel = feed['im:name']['label']
+        # 収録者
+        author = feed['im:artist']['label']
+        # リリース日
+        released_at = feed['im:releaseDate']['attributes']['label']
+        tmp = {
+            'title': title,
+            'channel': channel,
+            'author': author,
+            'released_at': released_at
+        }
+        feeds.append(tmp)
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(feeds)
     context = {
-        'feeds': res['feed']['entry']
+        'feeds': feeds
     }
 
     return render(request, 'feed/index.html', context)
